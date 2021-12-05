5abac27c6be9ef2b3486928f16b53ffd62413e7a
Okan <okso2004@gmail.com>
2021-12-05 20:34:00 +0100

    Added option for 4 unknowns & added floats

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-'''
-cheatsheet:
-https://numpy.org/doc/stable/reference/generated/numpy.linalg.solve.html
-
->>> a = np.array([[1, 2], [3, 5]])
->>> b = np.array([1, 2])
->>> x = np.linalg.solve(a, b)
->>> x
-Output: array([-1.,  1.])
-'''
 from numpy import array, linalg
 from os import system
 
@@ -18,12 +8,12 @@ def solve_two_unknowns():
 
     print("a*x + b*y = c \nd*x + e*y = f")
 
-    a = int(input("Enter value for factor a: "))    
-    b = int(input("Enter value for factor b: "))
-    c = int(input("Enter value for product c: "))    
-    d = int(input("Enter value for factor d: "))
-    e = int(input("Enter value for factor e: "))
-    f = int(input("Enter value for solution f: "))
+    a = float(input("Enter value for factor a: "))    
+    b = float(input("Enter value for factor b: "))
+    c = float(input("Enter value for product c: "))    
+    d = float(input("Enter value for factor d: "))
+    e = float(input("Enter value for factor e: "))
+    f = float(input("Enter value for solution f: "))
 
     x = array([[a, b], [d, e]])
     y = array([c, f])
@@ -36,24 +26,57 @@ def solve_three_unknowns():
     
     print("a*x + b*y + c*z = d \ne*x + f*y + g*z = h \ni*x + j*y + k*z = l")
 
-    a = int(input("Enter value for factor a: "))    
-    b = int(input("Enter value for factor b: "))
-    c = int(input("Enter value for factor c: "))    
-    d = int(input("Enter value for solution d: "))
-    e = int(input("Enter value for factor e: "))
-    f = int(input("Enter value for factor f: "))
-    g = int(input("Enter value for factor g: "))
-    h = int(input("Enter value for solution h: "))
-    i = int(input("Enter value for solution i: "))
-    j = int(input("Enter value for factor j: "))
-    k = int(input("Enter value for factor k: "))
-    l = int(input("Enter value for solution l: "))
+    a = float(input("Enter value for factor a: "))    
+    b = float(input("Enter value for factor b: "))
+    c = float(input("Enter value for factor c: "))    
+    d = float(input("Enter value for solution d: "))
+    e = float(input("Enter value for factor e: "))
+    f = float(input("Enter value for factor f: "))
+    g = float(input("Enter value for factor g: "))
+    h = float(input("Enter value for solution h: "))
+    i = float(input("Enter value for solution i: "))
+    j = float(input("Enter value for factor j: "))
+    k = float(input("Enter value for factor k: "))
+    l = float(input("Enter value for solution l: "))
 
     x = array([[a, b, c], [e, f, g], [i, j, k]])
     y = array([d, h, l])
     solution = linalg.solve(x, y)
 
     print(f"The solutions are x={solution[0]}; y={solution[1]}; z={solution[2]}.")
+
+
+def solve_four_unknowns():
+    
+    print("a*x + b*y + c*z + d*w = e \nf*x + g*y + h*z + i*w = j \nk*x + l*y + m*z + n*w = o \np*x + q*y + r*z + s*w = t")
+
+    a = float(input("Enter value for factor a: "))    
+    b = float(input("Enter value for factor b: "))
+    c = float(input("Enter value for factor c: "))    
+    d = float(input("Enter value for factor d: "))
+    e = float(input("Enter value for solution e: "))
+    f = float(input("Enter value for factor f: "))
+    g = float(input("Enter value for factor g: "))
+    h = float(input("Enter value for factor h: "))
+    i = float(input("Enter value for factor i: "))
+    j = float(input("Enter value for solution j: "))
+    k = float(input("Enter value for factor k: "))
+    l = float(input("Enter value for factor l: "))
+    m = float(input("Enter value for factor m: "))
+    n = float(input("Enter value for factor n: "))
+    o = float(input("Enter value for solution o: "))
+    p = float(input("Enter value for factor p: "))
+    q = float(input("Enter value for factor q: "))
+    r = float(input("Enter value for factor r: "))
+    s = float(input("Enter value for factor s: "))
+    t = float(input("Enter value for solution t: "))
+
+    x = array([[a, b, c, d], [f, g, h, i], [k, l, m, n], [p, q, r, s]])
+    y = array([e, j, o, t])
+    solution = linalg.solve(x, y)
+
+    print(f"The solutions are x={solution[0]}; y={solution[1]}; z={solution[2]}; w={solution[3]}.")
+
 
 def main():
 
@@ -63,6 +86,8 @@ def main():
         solve_two_unknowns()
     if choice == 3:
         solve_three_unknowns()
+    if choice == 4:
+        solve_four_unknowns()
 
 
 if __name__ == '__main__':
