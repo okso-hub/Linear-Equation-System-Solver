a7bd2957c95f8e3515e011589e175d71346e3660
Okan <okso2004@gmail.com>
2021-12-05 13:27:28 +0100

    Added option to solve for three unknowns

--- main.py
+++ main.py
@@ -33,11 +33,36 @@ def solve_two_unknowns():
 
 
 def solve_three_unknowns():
-    pass
+    
+    print("a*x + b*y + c*z = d \ne*x + f*y + g*z = h \ni*x + j*y + k*z = l")
 
+    a = int(input("Enter value for factor a: "))    
+    b = int(input("Enter value for factor b: "))
+    c = int(input("Enter value for factor c: "))    
+    d = int(input("Enter value for solution d: "))
+    e = int(input("Enter value for factor e: "))
+    f = int(input("Enter value for factor f: "))
+    g = int(input("Enter value for factor g: "))
+    h = int(input("Enter value for solution h: "))
+    i = int(input("Enter value for solution i: "))
+    j = int(input("Enter value for factor j: "))
+    k = int(input("Enter value for factor k: "))
+    l = int(input("Enter value for solution l: "))
+
+    x = array([[a, b, c], [e, f, g], [i, j, k]])
+    y = array([d, h, l])
+    solution = linalg.solve(x, y)
+
+    print(f"The solutions are x={solution[0]}; y={solution[1]}; z={solution[2]}")
 
 def main():
-    solve_two_unknowns()
+
+    choice = int(input("How many unknowns are you looking for? "))
+
+    if choice == 2:
+        solve_two_unknowns()
+    if choice == 3:
+        solve_three_unknowns()
 
 
 if __name__ == '__main__':
